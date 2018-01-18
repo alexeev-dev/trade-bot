efe1603ce90146e2850b916e0ad1d2bae9207cda
Вова <vladimir-shalaev22@yandex.ru>
2018-01-19 00:16:54 +0700

    Добавляет заготовку в проект

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Trade Bot</h1>
+        Всё готово. Можно начинать работать!
+      </div>
+    )
+  }
+}
+
+export default App
